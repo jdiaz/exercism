ca4b3f82ea4ddd7616f359bf9d3d8eaa60427b31
Jose Diaz <jrd.palacios@gmail.com>
2018-12-27 16:10:28 -0600

    Replacing implementation with calculous formulas

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -1,25 +1,15 @@
 // Package diffsquares contains functions to calculate SquareOfSum, SumOfSquares, and their difference.
 package diffsquares
 
-import "math"
-
 // SquareOfSum computes the square of the sum of the first n numbers.
 func SquareOfSum(n int) int {
-	sum := 0
-	for i := 1; i <= n; i++ {
-		sum += i
-	}
+	sum := (n * (n + 1)) / 2
 	return sum * sum
 }
 
-// SquareOfSum computes the sum of squares of the first n numbers.
+// SumOfSquares computes the sum of squares of the first n numbers.
 func SumOfSquares(n int) int {
-	fn := float64(n)
-	total := 0.0
-	for i := 1.0; i <= fn; i++ {
-		total += math.Pow(i, 2)
-	}
-	return int(total)
+	return (n * (n + 1) * (2*n + 1) / 6)
 }
 
 // Difference computes the difference between SquareOfSum and SumOfSquares values.
