10efbb5ecf678454c9646ee2c1831043d0fe88d8
Jose Diaz <jrd.palacios@gmail.com>
2018-12-28 14:05:58 -0600

    consolidating the two functions into one

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -9,38 +9,28 @@ import (
 // Valid determines if a credit card number is valid or not.
 func Valid(creditcard string) bool {
 	creditcard = strings.Replace(creditcard, " ", "", -1)
-	if !isValidCreditCardNumberString(creditcard) {
+	n := len(creditcard)
+	if n <= 1 {
 		return false
 	}
 
 	total := 0
-	for pos, count := len(creditcard)-1, 0; pos >= 0; pos, count = pos-1, count+1 {
-		digit := int(creditcard[pos] - '0')
+	count := 0
+	for pos := n - 1; pos >= 0; pos-- {
+		c := rune(creditcard[pos])
+		if !unicode.IsDigit(c) {
+			return false
+		}
+		digit := int(c - '0')
 		if count%2 != 0 { // is odd
 			digit = digit * 2
 			if digit > 9 {
-				digit = digit - 9
+				digit -= 9
 			}
 		}
 		total += digit
+		count++
 	}
 
-	if total%10 == 0 {
-		return true
-	}
-
-	return false
-}
-
-func isValidCreditCardNumberString(s string) bool {
-	if len(s) <= 1 {
-		return false
-	}
-
-	for _, char := range s {
-		if !unicode.IsDigit(char) {
-			return false
-		}
-	}
-	return true
+	return total%10 == 0
 }
