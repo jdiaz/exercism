7fa85fc686131d9e29eb279e361de86405c3d827
Jose Diaz <jrd.palacios@gmail.com>
2018-12-30 15:22:20 -0600

    word count implementation

---
+++ go/word-count/word_count.go
@@ -0,0 +1,39 @@
+// Package wordcount contains functions for counting words in a string.
+package wordcount
+
+import (
+	"strings"
+	"unicode"
+)
+
+// Frequency is a map of words associated with the number of times they show up in a string.
+type Frequency = map[string]int
+
+// WordCount determines the ocurrance of each word in string.
+func WordCount(s string) Frequency {
+	freq := make(Frequency, 0)
+	buf := make([]rune, 0)
+	for _, c := range strings.ToLower(s) {
+		if unicode.IsLetter(c) || c == '\'' || unicode.IsDigit(c) {
+			buf = append(buf, c)
+			continue
+		}
+		// Not valid char delimiter; count word
+		n := len(buf)
+		if n > 0 {
+			if buf[0] == '\'' && buf[n-1] == '\'' {
+				// Remove single quotes
+				buf = buf[1 : n-1]
+			}
+			freq[string(buf)]++
+			buf = make([]rune, 0)
+		}
+
+	}
+	// Check for last word
+	if len(buf) > 0 {
+		freq[string(buf)]++
+	}
+
+	return freq
+}
