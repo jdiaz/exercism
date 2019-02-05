bae7d465b863f4250ffe28c64fa9b2c5259c1be1
Jose Diaz <jrd.palacios@gmail.com>
2019-02-04 22:09:05 -0600

    Implemented clock

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -1,9 +1,7 @@
 // Package clock contains functions pertaining to the implementation of a time-only clock.
 package clock
 
-import (
-	"fmt"
-)
+import "fmt"
 
 const hourInDay = 24
 const minutesInHour = 60
@@ -17,14 +15,14 @@ type Clock struct {
 // New initializes a clock with default hour and minutes values
 func New(hr, mnts int) Clock {
 	hr = hr % hourInDay
-	mnts = mnts % minutesInDay
 	if hr < 0 {
 		hr = hr + hourInDay
 	}
+	mnts = mnts % minutesInDay
 	if mnts < 0 {
 		mnts = mnts + minutesInDay
 	}
-	minutes := hr * minutesInHour + mnts
+	minutes := hr*minutesInHour + mnts
 	if minutes >= minutesInDay {
 		minutes = minutes % minutesInDay
 	}
@@ -33,13 +31,9 @@ func New(hr, mnts int) Clock {
 
 // String returns a string representation of the clock
 func (c Clock) String() string {
-	fmt.Println("=====")
-	fmt.Println(c.minutes)
 	hour := c.minutes / minutesInHour
 	minutes := c.minutes % minutesInHour
-	s := fmt.Sprintf("%02d:%02d", hour, minutes)
-	fmt.Println(s)
-	return s
+	return fmt.Sprintf("%02d:%02d", hour, minutes)
 }
 
 // Add increments the time by the provides minutes amount
